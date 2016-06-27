0913adb4a3daad4bbd23ab8ce8dca13605b125d6
Student <Student@G306APC19.manila.dlsu.edu.ph>
2016-06-27 15:22:13 +0800

    Hi Firends

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -16,7 +16,7 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Swag");
+        System.out.println("Swaggers");
     }
     
 }
